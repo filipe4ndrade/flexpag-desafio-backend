cf5ac0027bc27f97c1a2e8157bd001b94b247929
Filipe <filipeandrade000@gmail.com>
2023-03-02 08:13:58 -0300

    Excecoes do Controller

---
+++ src/main/java/com/flexpag/paymentscheduler/controllers/exceptions/StandardError.java
@@ -0,0 +1,31 @@
+package com.flexpag.paymentscheduler.controllers.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import lombok.Data;
+
+@Data
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+		
+	}
+
+	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+	
+	
+}
